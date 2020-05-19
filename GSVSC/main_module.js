bdab88fa0daac430765098eac565ad7955eb590a
Astro-Sola <63591157+Astro-Sola@users.noreply.github.com>
2020-05-19 20:06:16 +0900

    remaked

--- GSVSC/main_module.js
+++ GSVSC/main_module.js
@@ -73,110 +73,30 @@ function init(){
 
   
   //ガイド
-  var grid = new THREE.GridHelper(1000, 10);
+  var grid = new THREE.GridHelper(4000, 20);
   scene.add(grid);
 
 
 
-  //恒星の色温度の関数
-  function starTemperatureColor(seed){
-    var seed, hex;
-    if (seed < 0.7645) {
-      hex = 0xffcc6f;
-    } else if (seed < 0.8855){
-      hex = 0xffd2a1;
-    } else if (seed < 0.9615) {
-      hex = 0xfff4ea;
-    } else if (seed < 0.9915) {
-      hex = 0xf8f7ff;
-    } else if (seed < 0.9975) {
-      hex = 0xcad7ff;
-    } else if (seed < 0.9985) {
-      hex = 0xaabfff;
-    } else if (seed < 0.9985003) {
-      hex = 0x9bbff;
-    } else {
-      hex = 0xff9956;
-    }
-    return hex;    
-  }
-
-
-  //恒星の追加を行う関数
-  function generateMobStars(){
-    //テクスチャ指定
-    var loader = new THREE.TextureLoader();
-    var texture = loader.load('img/star.png');
-    //モブ恒星の数と範囲
-    var STARSUM = 100000;//星の総数
-    var STARSPREAD = 2000;//星の広がり
-    //モブ恒星のジオメトリ
-    var mobStarGeomrtry = new THREE.Geometry();
-    //モブ恒星マテリアル
-    var mobStarMaterial = new THREE.PointsMaterial({
-      size: 20,
-      alphaMap: texture,
-      vertexColors: true,
-      transparent: true,
-      blending: THREE.NormalBlending,
-      depthTest: false,
-      //depthWrite: false,
-    });
-        for (var i = 0; i < STARSUM; i++) {
-          var seed = Math.random();
-          starColor = starTemperatureColor(seed);
-          var x,y; 
-          //極座標パラメータ
-          var r = Math.random();
-          var z = getRandomArbitrary(-1, 1);
-          var phi = getRandomArbitrary(0, 2 * Math.PI);
-          //直交座標系へ返還し設置位置として格納
-          mobStarGeomrtry.vertices.push(
-            new THREE.Vector3(
-            x = Math.cbrt(r) * STARSPREAD * Math.sqrt(1 - z * z) * Math.cos(phi),
-            y = Math.cbrt(r) * STARSPREAD * Math.sqrt(1 - z * z) * Math.sin(phi),
-            z = Math.cbrt(r) * STARSPREAD * z
-            )
-        );
-      mobStarGeomrtry.colors.push(
-        new THREE.Color(starColor)
-        );
-    }
-    var mobStar = new THREE.Points(mobStarGeomrtry, mobStarMaterial);
-    scene.add(mobStar);
-  }
-  generateMobStars();
-
-  //ネームドスターの追加
-  function genarateNamedStars() {
-    //テクスチャ指定
-    var loader = new THREE.TextureLoader();
-    var texture = loader.load('img/star.png');
+  //ネームドスターの生成
+  function GenerateNamedStars(){
     //ジオメトリの作成
-    var namedStarGeometry = new THREE.PlaneGeometry();
+    var geometry = new THREE.IcosahedronGeometry( 1, 2 );
     //マテリアルの作成
-    var namedStarMaterial = new THREE.Material();
-
-
-    //demo.jsonファイルの読み込み
-    $(function(){
-      $.getJSON('demo.json', function(data){
-      var len = data.length;
-      //jsonファイルの要素の数だけ繰り返す
-      for (var i = 0; i < len; i++) {
-
-
-
-
-
-
-
-
-        }
-      })
-    })
+    var material = new THREE.MeshBasicMaterial( { color: 0xffeecc } );
+    //メッシュを特定個数作成
+    for (var i = 0; i < 1000; i++) {
+      var star = new THREE.Mesh( geometry, material );
+        star.position.x = ((Math.random() - 0.5) * 100);
+        star.position.y = ((Math.random() - 0.5) * 100);
+        star.position.z = ((Math.random() - 0.5) * 100);
+        console.log(i);
+        //モデル読み込み
+        scene.add( star );
+    }
   }
 
+  GenerateNamedStars();
 
 
   //毎フレーム時の更新
