8b6cff35a13b662632fcba623c721980aa60eecf
Astro-Sola <63591157+Astro-Sola@users.noreply.github.com>
2020-09-16 01:46:41 +0900

    Update main_module.js

--- GSVSC/main_module.js
+++ GSVSC/main_module.js
@@ -113,6 +113,7 @@ function CSVtoArrayConverter(string){
 
 /*スプライト生成関数ここから*/
 function generateSprite(group, objectList){
+  console.log("fuck");
   for(var i=1;i<objectList;i++){
     var sprite = new THREE.Sprite(new THREE.SpriteMaterial({ color: objectList[i][4] }));
     sprite.position.set(objectList[i][1], objectList[i][2], objectList[i][3]);
