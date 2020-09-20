0d0bfea4d0af3e42927196c7595388fe8c87545d
Astro-Sola <63591157+Astro-Sola@users.noreply.github.com>
2020-09-20 20:52:26 +0900

    Update main_module.js

--- GSVSC/main_module.js
+++ GSVSC/main_module.js
@@ -92,7 +92,7 @@ function main(){
             intersects[0].object.material.color.set(0xff0000);
           } else {
             console.log("None intersected!");
-            console.log(intersects[0].object);
+            console.log(intersects[0]);
           }
         });
         cameraControl.update();
