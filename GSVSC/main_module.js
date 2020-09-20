c847a648fe531b71848d5e6240fb71be2c101927
Astro-Sola <63591157+Astro-Sola@users.noreply.github.com>
2020-09-20 19:48:01 +0900

    Update main_module.js

--- GSVSC/main_module.js
+++ GSVSC/main_module.js
@@ -87,7 +87,7 @@ function main(){
         objectGroup.children.map(sprite => {
           //交差しているオブジェクトが１つ以上あって、それが最前面
           if(intersects.length > 0 && sprite === intersects[0].object){
-            materialColor = selectedObject.material.color.clone();
+            var materialColor = selectedObject.material.color.clone();
             sprite.material.color.set( 0xff0000 );
           } else {
             sprite.material.color.set( materialColor );
