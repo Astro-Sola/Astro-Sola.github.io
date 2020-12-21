01dbdd10e896289b6ef1a43ad93541c14baf3563
Astro-Sola <63591157+Astro-Sola@users.noreply.github.com>
2020-12-21 22:02:39 +0900

    Update main_module.js

--- GSVSC/main_module.js
+++ GSVSC/main_module.js
@@ -32,6 +32,7 @@ function main(){
       camera.lookAt(scene.position);
       //カメラコントロールの設定
       var cameraControl = new THREE.TrackballControls(camera, renderer.domElement);
+      /*
       cameraControl.rotateSpeed = 1.2;
       cameraControl.zoomSpeed = 0.6;
       //カメラの入力キーの設定
@@ -40,6 +41,7 @@ function main(){
       //カメラの最大最小距離の設定
       cameraControl.minDistance = 1;
       cameraControl.maxDistance = 1000;
+      */
 
       //グループの生成
       var objectGroup = new THREE.Group();
