e83ca878535101db08d657663d9ae5f68c919014
Astro-Sola <63591157+Astro-Sola@users.noreply.github.com>
2021-12-24 00:35:01 +0900

    0.0.1

--- Helios/js/index.js
+++ Helios/js/index.js
@@ -2,9 +2,12 @@ window.onload = function () {
     main();
 }
 function main(){
-    const mass = document.getElementById("mass");
-    let mass_sol = document.getElementById("mass_sol");
-    let radius = document.getElementById("radius");
-    mass_sol.innerHTML = mass.value / 1.9884e+30;
-    radius.innerHTML = mass.value;
+    const mass = document.getElementById("mass").value;
+    if( mass < 0.1 ){window.alert("too low mass! It can't be star! (mass ≦ 0.1)")}
+    let mass_mks = document.getElementById("mass_mks");
+    let luminosity = document.getElementById("luminosity");
+    let luminosity_mks = document.getElementById("luminosity_mks");
+    mass_mks.innerHTML = (mass * 1.9884e+30).toExponential(4);
+    luminosity.innerHTML = Math.pow(Number(mass), 3.9).toExponential(4);
+    luminosity_mks.innerHTML = (mass * 3.828e+26).toExponential(4);
 }
